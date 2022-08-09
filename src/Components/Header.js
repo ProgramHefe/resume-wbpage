c0aa4f2e0f61e3f8bfb77efd01082b34dc626f12
ProgramHefe <h.kosonen91@hotmail.com>
2022-08-09 13:05:50 +0300

    Additions to styles, App and Header

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../styles.css'
+
+function Header() {
+
+    return(
+        <div className="Header">
+            <p>Heikki Kosonen</p>
+            <p>Kiikkulankatu 1 as 12</p>
+            <p>Puh: +348445777077</p>
+        </div>
+    );
+}
+
+export default Header;
