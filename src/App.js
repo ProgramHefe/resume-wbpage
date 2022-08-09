bf668c13f75ab901cc5df02cfee64d0aa177412c
ProgramHefe <h.kosonen91@hotmail.com>
2022-08-09 14:36:04 +0300

    Style changes, education added.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header'
 import Introduction from './Components/Introduction'
-
+import Education from './Components/Education';
 function App() {
   return (
     <div className="App">
       <Header />
+      <Education />
       <Introduction />
+      
     </div>
   );
 }
---
+++ src/Components/Education.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Picture from '../Images/hefeeeeee.jpg'
+function Education () {
+
+    return(
+        <div className="Education">
+            <img src={Picture} />
+            <h3>Tradenomi - Tietojenkäsittely</h3>
+            <h3>LAB- university of applied sciences</h3>
+        </div>
+    )
+}
+
+export default Education;
