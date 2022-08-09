55098bf7b201c0799b9858e12bd9d433f038a460
ProgramHefe <h.kosonen91@hotmail.com>
2022-08-09 15:25:42 +0300

    footer made

---
+++ src/Components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+
+    return (
+        <div>
+            <div className="Footer">
+                <a href="https://www.linkedin.com/in/heikki-kosonen-0981a9241/">Linkedin</a>
+                <a href="https://github.com/ProgramHefe">Github</a>
+                <a href="https://www.instagram.com/heikkikos/">Instagram</a>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
