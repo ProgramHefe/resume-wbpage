29889479bb8daf53907228d1adb87c3e1ad1d677
ProgramHefe <h.kosonen91@hotmail.com>
2022-08-09 14:14:04 +0300

    styles changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header'
+import Introduction from './Components/Introduction'
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <Introduction />
     </div>
   );
 }
---
+++ src/Components/Introduction.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+function Introduction () {
+
+    return(
+        <div className="Introduction">
+            <p>Moi. Olen Heikki Kosonen, 30-vuotias aloitteleva IT-alan 
+               ammattilainen Lahdesta. Olen valmistumassa tietojenkäsittelyn opinnoista
+               syyskuussa 2022 ja olisin halukas aloittamaan työurani koodaamisen parissa.
+               Kokemusta minulla on opintojeni kautta C#, MySQL, JavaScript, React, HTML5
+               ,CSS3 ja Azure pilviratkaisuista. Harjoittelussani pääsin vahvistamaan osaamistani
+               web-kehityksestä ja siellä käytettiin React, Javascript, HTML5 ja CSS3. Tietokantana
+               harjoittelussa toimi MongoDB ja homma pyöri gitin ja AWS:n kautta. olen avoin ja yhteistyö
+               kykyinen työntekijä, jota motivoi suuresti kehittäminen ja uuden oppiminen tällä alalla. 
+               Harrastuksinani vapaa-ajalla toimii kuntosali, maastopyöräily, padel/tennis, lukeminen ja 
+               videopelit.
+            </p>
+        </div>
+    )
+}
+
+export default Introduction;
