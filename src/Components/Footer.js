a14f1d27c01e21efc79b6a66278d5f6244ace008
ProgramHefe <h.kosonen91@hotmail.com>
2022-08-10 11:19:31 +0300

    Footer changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,9 +5,15 @@ function Footer() {
     return (
         <div>
             <div className="Footer">
+                <p>
                 <a href="https://www.linkedin.com/in/heikki-kosonen-0981a9241/">Linkedin</a>
+                </p>
+                <p>
                 <a href="https://github.com/ProgramHefe">Github</a>
+                </p>
+                <p>
                 <a href="https://www.instagram.com/heikkikos/">Instagram</a>
+                </p>
             </div>
         </div>
     );
